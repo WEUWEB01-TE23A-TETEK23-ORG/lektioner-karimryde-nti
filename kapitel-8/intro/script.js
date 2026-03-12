8b3ba019ed16a76728eb5385e7c6ec1e2535e142
Karim Ryde <you@example.com>
2026-03-12 10:51:43 +0100

    Tränat på variabler och att skriva ut med alert(); ställa frågor

---
+++ kapitel-8/intro/script.js
@@ -0,0 +1,28 @@
+//alert("Hej på dig!")
+
+// let tal1 = 5
+// let tal2 = 7
+// let summa = tal1 + tal2
+// alert(tal1 + " + " + tal2 + " = " + summa)
+
+// let fnamn = "Karim"
+// let enamn = "Ryde"
+// alert(fnamn + " " + enamn)
+
+// Skapa en variabel ålder med er ålder
+// Skriv ut en mening:
+// "Jag heter XX YY och är ZZ år gammal!"
+// let ålder = 60
+// alert("Jag heter " + fnamn + " " + enamn + " och är " + ålder + " år gammal!")
+// console.log("Hejhej");
+
+//let svar = prompt("Vad heter du?")
+// console.log(svar);
+
+// Ställ en klurig fråga till användaren (inte robot)
+svar = prompt("Vad är 12 + 23?")
+if (svar == "35") {
+    alert("Välkommen till min webbsida")
+} else {
+    alert("Hejdå!")
+}
