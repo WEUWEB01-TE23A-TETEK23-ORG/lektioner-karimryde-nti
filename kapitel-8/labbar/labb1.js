015e81805bbba7bc89a7bbaef006e63438541583
Karim Ryde <you@example.com>
2026-03-25 11:30:42 +0100

    Tränat på att knappar, onclick och styling

---
+++ kapitel-8/labbar/labb1.js
@@ -0,0 +1,20 @@
+function andraBakgrund() {
+    // Ge body css för ljusgrönt
+    document.body.style.backgroundColor = "lightgreen"
+}
+function andraRubrik() {
+    document.querySelector("h1").style.color = "red"
+    document.querySelector("h1").style.fontSize = "96px"
+}
+function gomText() {
+    document.querySelector(".hemlig-lada").style.display = "none"
+}
+function visaText() {
+    document.querySelector(".hemlig-lada").style.display = "block"
+}
+function blaText() {
+    document.querySelector(".hemlig-lada").style.color = "blue"
+}
+function gulBg() {
+    document.body.style.backgroundColor = "yellow"
+}
